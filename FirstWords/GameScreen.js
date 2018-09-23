e9a6356726721c16023022818dab78254cf6ffb1
Carl Olson <colson10@gmail.com>
2018-09-22 23:12:44 -0700

    added logic to randomize game data and pick a correct item and display that word

--- FirstWords/GameScreen.js
+++ FirstWords/GameScreen.js
@@ -1,13 +1,28 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, FlatList, TouchableHighlight} from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, FlatList, TouchableHighlight } from 'react-native';
 import gameDataArray from './game-data.js';
+
+const shuffle = (array) => {
+  let i = 0, j = 0, temp = null;
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array;
+}
 
 export default class App extends React.Component {
   state = {
-    correct: 'cat',
+    correct: '',
     winner: false,
     loser: false,
     newScreen: true,
+    gameStarted: false,
+    arrayToShow: [],
+    remaining: [],
   }
 
   handleSelection = (target) => {
@@ -18,25 +33,59 @@ export default class App extends React.Component {
     }
   }
 
+  handleStartGame = () => {
+    // const randomIndex = Math.floor(Math.random() * ((gameDataArray.length - 1) - 1));
+    // console.log(randomIndex);
+    console.log(gameDataArray[0], 'first index');
+    const data = shuffle(gameDataArray);
+    console.log(data,);
+    const first = data.pop();
+    console.log(first);
+    const remaining = [];
+    remaining.push(first);
+
+    for (let i = 0; i < 3; i++) {
+      const lastItemRemoved = data.pop();
+      remaining.push(lastItemRemoved);
+    }
+    const shuffled = shuffle(remaining);
+    console.log(remaining, data);
+
+    this.setState({ gameStarted: true, winner: false, loser: false, correct: first.title, remaining: shuffled })
+  }
+
   render() {
     console.log(this.state);
     return (
-      <View style={styles.container}>
-        {this.state.winner && <Text style={styles.welcome}>That is correct!</Text>} 
-        {this.state.loser && <Text style={styles.welcome}>'Sorry, play again'</Text>}
-         <FlatList
-          data={gameDataArray}
-          renderItem={({item}) => (
-          <View >
-            <TouchableHighlight onPress={() => this.handleSelection(item)} value={item.title}>
-            <Image  source={{uri:item.url}} style={styles.image}/>
-            </TouchableHighlight>
-          </View>
-          )     
+      <React.Fragment>
+        {
+          !this.state.gameStarted 
+            && <View>
+              <TouchableHighlight>
+                <Text onPress={this.handleStartGame}>Start Game!</Text>
+              </TouchableHighlight>
+            </View>
         }
-
-        />
-      </View>
+        {
+          this.state.gameStarted && 
+          <View style={styles.container}>
+            {this.state.winner && <Text style={styles.welcome}>That is correct!</Text>} 
+            {this.state.loser && <Text style={styles.welcome}>'Sorry, play again'</Text>}
+            <FlatList
+              data={this.state.remaining}
+              renderItem={({item}) => (
+              <View >
+                <TouchableHighlight onPress={() => this.handleSelection(item)} value={item.title}>
+                <Image  source={{uri:item.url}} style={styles.image}/>
+                </TouchableHighlight>
+              </View>
+              )     
+            }
+            />
+            <Text>{this.state.correct}</Text>
+          </View>
+        }
+      </React.Fragment>
     );
   }
 }
--- FirstWords/game-data.js
+++ FirstWords/game-data.js
@@ -17,10 +17,35 @@ const gameData = [
     letter: 'C',
   },
   {
-    title: 'dolphin',
-    url: 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    title: 'dog',
+    url: 'https://images.pexels.com/photos/257540/pexels-photo-257540.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     letter: 'D',
   },
+  {
+    title: 'baby',
+    url: 'https://images.pexels.com/photos/161709/newborn-baby-feet-basket-161709.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    letter: 'B',
+  },
+  {
+    title: 'car',
+    url: 'https://images.pexels.com/photos/627678/pexels-photo-627678.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    letter: 'C',
+  },
+  {
+    title: 'elephant',
+    url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    letter: 'E',
+  },
+  {
+    title:'tractor',
+    url: 'https://images.pexels.com/photos/327378/pexels-photo-327378.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    letter: 'T',
+  },
+  {
+    title: 'pizza',
+    url: 'https://images.pexels.com/photos/708587/pexels-photo-708587.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    letter:'P',
+  }
 ]
 
 export default gameData;
