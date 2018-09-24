5570d0e19148cbf1f8496f6b22b6f9079bbd1d5d
Carl Olson <colson10@gmail.com>
2018-09-23 22:49:48 -0700

    added styling for image buttons and background

--- FirstWords/App.js
+++ FirstWords/App.js
@@ -33,7 +33,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    // backgroundColor: '#F5FCFF',
+    backgroundColor: '#C390D4',
   },
   welcome: {
     fontSize: 20,
--- FirstWords/GameScreen.js
+++ FirstWords/GameScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, FlatList, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, ImageBackground } from 'react-native';
 import gameDataArray from './game-data.js';
 
 const shuffle = (array) => {
@@ -22,6 +22,7 @@ export default class App extends React.Component {
     newScreen: true,
     gameStarted: false,
     arrayToShow: [],
+    currentSet: [],
     remaining: [],
   }
 
@@ -34,13 +35,8 @@ export default class App extends React.Component {
   }
 
   handleStartGame = () => {
-    // const randomIndex = Math.floor(Math.random() * ((gameDataArray.length - 1) - 1));
-    // console.log(randomIndex);
-    console.log(gameDataArray[0], 'first index');
     const data = shuffle(gameDataArray);
-    console.log(data,);
     const first = data.pop();
-    console.log(first);
     const remaining = [];
     remaining.push(first);
 
@@ -49,20 +45,25 @@ export default class App extends React.Component {
       remaining.push(lastItemRemoved);
     }
     const shuffled = shuffle(remaining);
-    console.log(remaining, data);
 
-    this.setState({ gameStarted: true, winner: false, loser: false, correct: first.title, remaining: shuffled })
+    this.setState({ gameStarted: true, winner: false, loser: false, correct: first.title, currentSet: shuffled, remaining: data })
   }
 
   render() {
-    console.log(this.state);
+    const { currentSet } = this.state;
     return (
+      <ImageBackground source={{uri: 'https://images.pexels.com/photos/1020317/pexels-photo-1020317.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'}} style={{width: '100%', height: '100%'}}>
       <React.Fragment>
         {
           !this.state.gameStarted 
             && <View>
-              <TouchableHighlight>
-                <Text onPress={this.handleStartGame}>Start Game!</Text>
+              <TouchableHighlight
+                onPress={this.handleStartGame} 
+                color="#841584"
+                style={styles.button}>
+                <Text>
+                  Start Game!
+                </Text>
               </TouchableHighlight>
             </View>
         }
@@ -71,45 +72,97 @@ export default class App extends React.Component {
           <View style={styles.container}>
             {this.state.winner && <Text style={styles.welcome}>That is correct!</Text>} 
             {this.state.loser && <Text style={styles.welcome}>'Sorry, play again'</Text>}
-            <FlatList
-              data={this.state.remaining}
-              renderItem={({item}) => (
-              <View >
-                <TouchableHighlight onPress={() => this.handleSelection(item)} value={item.title}>
-                <Image  source={{uri:item.url}} style={styles.image}/>
-                </TouchableHighlight>
-              </View>
-              )     
-            }
-            />
-            <Text>{this.state.correct}</Text>
+
+            <View style={styles.topSet}>
+              <TouchableHighlight onPress={() => this.handleSelection(currentSet[0])} value={currentSet[0].title}>
+                <Image  source={{uri:currentSet[0].url}} style={styles.image}/>
+              </TouchableHighlight>
+              <TouchableHighlight onPress={() => this.handleSelection(currentSet[1])} value={currentSet[1].title}>
+                <Image  source={{uri:currentSet[1].url}} style={styles.image}/>
+              </TouchableHighlight>
+            </View>
+            <View style={styles.bottomSet}>
+              <TouchableHighlight onPress={() => this.handleSelection(currentSet[2])} value={currentSet[2].title}>
+                <Image  source={{uri:currentSet[2].url}} style={styles.image}/>
+              </TouchableHighlight>
+              <TouchableHighlight onPress={() => this.handleSelection(currentSet[3])} value={currentSet[3].title}>
+                <Image  source={{uri:currentSet[3].url}} style={styles.image}/>
+              </TouchableHighlight>
+            </View>
+            <Text style={styles.wordAnswer}>{this.state.correct.toUpperCase()}</Text>
           </View>
         }
       </React.Fragment>
+      </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    // backgroundColor: '#C390D4',
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    marginTop: 50,
+    alignContent: 'space-around',
+    // backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    position: 'absolute',
+    fontSize: 24,
+    top: '45%',
     textAlign: 'center',
-    margin: 10,
   },
   instructions: {
+    position: 'absolute',
+    fontSize: 32,
+    top: '50%',
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  button: {
+    borderColor: '#999999',
+    borderRadius: 10,
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 50,
+    paddingRight: 50,
+
+    backgroundColor: '#A1D490',
+  },
+  topSet: {
+    flex: 1,
+    width: '100%',
+    // backgroundColor: '#C390D4',
+    flexDirection: 'row',    
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  bottomSet: {
+    // backgroundColor: '#A1D490',
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
   },
   image: {
-    width: 50,
-    height: 50,
+    borderRadius: 10,
+    alignContent: 'space-around',
+    justifyContent: 'space-between',
+    width: 175,
+    height: 125,
+    borderWidth: 5,
+    borderColor: '#d6d7da',
   },
+  highlight: {
+    borderColor: '#A1D490',
+  },
+  wordAnswer: {
+    position: 'absolute',
+    top: '47.5%',
+    fontSize: 24,
+  }
 });
